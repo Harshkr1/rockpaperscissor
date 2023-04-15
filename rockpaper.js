1523b85f1c4b69bb018a86fd7dfff41569b3a4f8
Harsh Kumar <Harshkr70@gmail.com>
2023-04-15 22:49:17 +0530

    added the main file just having small eroor which i will recity witht he problem in my logic

--- rockpaper.js
+++ rockpaper.js
@@ -3,54 +3,86 @@ function getComputerChoice() {
     /* this Math.random function generatees a random number between 0 and 1 here 
      we  have set our conddition according to that here and Math.floor Rounds off to ther nearest decimal number  */
     let randomnumber = Math.random();
+    console.log(randomnumber);
     if (randomnumber <= 0.33)
-        return 'Rock';
+        return 'rock';
     else if (randomnumber > 0.33 && randomnumber <= 0.66)
-        return 'Paper';
-    else
-        return 'Scissor ';
+        return 'paper';
+    else if (randomnumber > 0.66 )
+        return 'scissor ';
 }
-console.log(getComputerChoice());
+
 function getUserChoice() {
     //generate User choice 
-    let entry=prompt("ENTER YOUR CHOICE :");
+    let entry = prompt("ENTER YOUR CHOICE :");
     // we are bringing it down to lower case so that it becomes case insensitive using the mentioned function here 
-    let userChoice = entry.toLowerCase();
-
+    let userChoice = entry;
     return userChoice;
 }
-console.log(getUserChoice());
-let choice=getComputerChoice().toLowerCase();
-function Result(userChoice,choice)
-{
-    if(userChoice==choice)
+let a = getComputerChoice(); // convert;
+let b= getUserChoice();
+console.log(a);
+console.log(b);
+
+ function value(a) 
+ {
+    if(a.toLowerCase() === 'rock' )
     {
-        return 'Both Got Same choice hence Match Draw';
+        return 1;   
     }
-    //using lower case because we have made already this to Lower case here 
-    else if(userChoice==rock && choice==scissor)
+    else if(a.toLowerCase() === 'paper')
     {
-        return 'User wins as User have Rock and Computer have scissor';
+        return 2;
     }
-    else if(userChoice==scissor && choice==rock)
+    else if(a.toLowerCase()==='scissor')
     {
-        return 'Computer wins as User have Scissor and Computer have rock';
+        return 3;
     }
-    else if(userChoice==paper && choice==scissor)
+    else
     {
-        return 'Computer  wins as User have Paper and Computer have scissor';
-    }
-    else if(userChoice==scissor && choice==paper)
-    {
-        return 'User wins as User have Scissor and Computer have Paper';
-    }
-    else if(userChoice== rock&& choice==paper)
-    {
-        return 'Computer  wins as User have rock and Computer have paper';
-    }
-    else if(userChoice==paper && choice==rock)
-    {
-        return 'User wins as User have Paper and Computer have  Rock';
+        return 'Invalid Input Cannot fight ';
     }
 }
-console.log(Result());
+
+let computer= value(a);
+let user= value(b);
+console.log(computer);
+console.log(user);
+
+function Result(computer, user) {
+    if(computer===user)
+    {
+        return 'Both is Same Match Draw';
+    }
+    if(computer>user)
+    {
+        if(computer===3 && user===1)
+        {
+            return 'User Win Computer Lost ';
+        }
+        else{
+            return 'Computer Wins User lost ';
+        }
+    }
+    else if (computer<user)
+    {
+          if(computer===1 && user===3)
+          {
+            return 'Computer Win User Lost';
+          }
+          else
+          {
+            return 'User Win Computer Lost';
+          }
+    }
+    else
+    {
+        return 'No game Possible';
+    }
+    
+}
+
+let result = Result(computer, user);
+
+
+console.log(result);
