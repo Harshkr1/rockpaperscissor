90e5ef8b94d9d72c55e359dd67fa0928f4203fdd
Harsh Kumar <Harshkr70@gmail.com>
2023-04-16 00:13:20 +0530

    Carefully cleared the error of the main function by considering the String Length

--- rockpaper.js
+++ rockpaper.js
@@ -5,11 +5,11 @@ function getComputerChoice() {
     let randomnumber = Math.random();
     console.log(randomnumber);
     if (randomnumber <= 0.33)
-        return 'rock';
+        return 'Rock';
     else if (randomnumber > 0.33 && randomnumber <= 0.66)
-        return 'paper';
-    else if (randomnumber > 0.66 )
-        return 'scissor ';
+        return 'Paper';
+    else if (randomnumber > 0.66)
+        return 'Scissor';
 }
 
 function getUserChoice() {
@@ -20,69 +20,54 @@ function getUserChoice() {
     return userChoice;
 }
 let a = getComputerChoice(); // convert;
-let b= getUserChoice();
+let b = getUserChoice();
 console.log(a);
 console.log(b);
 
- function value(a) 
- {
-    if(a.toLowerCase() === 'rock' )
-    {
-        return 1;   
+function value(a) {
+    if (a.toLowerCase() === 'rock') {
+        return 1;
     }
-    else if(a.toLowerCase() === 'paper')
-    {
+    else if (a.toLowerCase() === 'paper') {
         return 2;
     }
-    else if(a.toLowerCase()==='scissor')
-    {
+    else if (a.toLowerCase() === 'scissor') {
         return 3;
     }
-    else
-    {
+    else {
         return 'Invalid Input Cannot fight ';
     }
 }
 
-let computer= value(a);
-let user= value(b);
+let computer = value(a);
+let user = value(b);
 console.log(computer);
 console.log(user);
 
 function Result(computer, user) {
-    if(computer===user)
-    {
+    if (computer === user) {
         return 'Both is Same Match Draw';
     }
-    if(computer>user)
-    {
-        if(computer===3 && user===1)
-        {
+    if (computer > user) {
+        if (computer === 3 && user === 1) {
             return 'User Win Computer Lost ';
         }
-        else{
+        else {
             return 'Computer Wins User lost ';
         }
     }
-    else if (computer<user)
-    {
-          if(computer===1 && user===3)
-          {
+    else if (computer < user) {
+        if (computer === 1 && user === 3) {
             return 'Computer Win User Lost';
-          }
-          else
-          {
+        }
+        else {
             return 'User Win Computer Lost';
-          }
+        }
     }
-    else
-    {
+    else {
         return 'No game Possible';
     }
-    
+
 }
 
-let result = Result(computer, user);
-
-
-console.log(result);
+console.log(Result(computer, user));
