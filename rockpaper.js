85a5198f8db96a0f55eda45b455e8488bb59026d
Harsh Kumar <Harshkr70@gmail.com>
2023-04-24 19:48:11 +0530

    added border so as to add CSS easily

--- rockpaper.js
+++ rockpaper.js
@@ -1,10 +1,6 @@
-
 let UserCount = 0;
 let ComputerCount = 0;
-
-
 function getComputerChoice() {
-
     /* this Math.random function generatees a random number between 0 and 1 here 
      we  have set our conddition according to that here and Math.floor Rounds off to ther nearest decimal number  */
     let randomnumber = Math.random();
@@ -21,33 +17,22 @@ const game = function () {
     const buttons = document.querySelectorAll('button');
     const Comp=document.querySelector('.Comp');
     const User=document.querySelector('.User');
-
-    
-
     buttons.forEach((button) => {
-
-        
-
         // and for each one we add a 'click' listener
         button.addEventListener('click', (e) => {
 
             let Compchance = getComputerChoice().toLowerCase();
             let UserChance = e.target.textContent.toLowerCase();
-            console.log(`UserChoice: ${UserChance} and Compchance: ${Compchance}`);
-
-
             if (Compchance === UserChance) {
-                console.log(`Match Draw As Both Same `);
             }
             else if ((UserChance === 'rock' && Compchance === 'scissor') || (UserChance === 'paper' && Compchance === 'rock') || (UserChance === 'scissor' && Compchance === 'paper')) {
                 UserCount++;
-                console.log(`User Win Computer Lost `);
             }
             else if ((UserChance === 'rock' && Compchance === 'paper') || (UserChance === 'paper' && Compchance === 'scissor') || (UserChance === 'scissor' && Compchance === 'rock')) {
                 ComputerCount++;
-                console.log(`Computer Win User Lost `);
             }
-        
+            let choice=document.querySelector('.choice');
+            choice.textContent=`User: ${UserChance}   Computer:${Compchance}`;
             Comp.textContent=  `Computer Score is ${ComputerCount}`;
             User.textContent=  `User Score is ${UserCount}`;
            
@@ -57,67 +42,26 @@ const game = function () {
     });
 
 }
+const Result=function()
+{
+    const result= document.querySelector('#Result');
+    const res= document.querySelector('.res');
+    result.addEventListener('click', ()=>{
+        if(UserCount>ComputerCount)
+        {
+            res.textContent=`User Win by ${UserCount-ComputerCount} points to Computer`;
+        }
+        else  if(UserCount<ComputerCount)
+        {
+            res.textContent=`Computer  Wins by ${ComputerCount-UserCount} points to User`;
+        }
+        else
+        {
+            res.textContent=`Game Tied with both Computer and User having ${UserCount} points.`;
+        }
+    })
+}
 game();
-
-// console.log(a);
-
-
-
-
-// function getUserChoice() {
-//     //generate User choice
-//     let entry = prompt("ENTER YOUR CHOICE :");
-//     let userChoice = entry;
-//     return userChoice;
-// }
-// let UserCount = 0;
-// let ComputerCount = 0;
-
-
-
-
-// function Result(comp, user) {
-//     let Compchance = comp.toLowerCase();
-//     let userchance = user.toLowerCase();
-//     if (Compchance === userchance) {
-//         return `User:${user} and computer :${comp},Match Draw As Both Same `;
-//     }
-//     else if ((userchance === 'rock' && Compchance === 'scissor') || (userchance === 'paper' && Compchance === 'rock') || (userchance === 'scissor' && Compchance === 'paper')) {
-//         UserCount++;
-//         return `User : ${user} and computer : ${comp} ,User Win Computer Lost `;
-//     }
-//     else if ((userchance === 'rock' && Compchance === 'paper') || (userchance === 'paper' && Compchance === 'scissor') || (userchance === 'scissor' && Compchance === 'rock')) {
-//         ComputerCount++;
-//         return `User : ${user} and computer : ${comp} ,Computer Win User Lost `;
-//     }
-//     else {
-//         return 'Invalid values cannot match .'
-//     }
-
-// }
-
-
-// function game() {
-//     let n = parseInt(prompt(`Enter the number of rounds you want to play :`));
-//     for (let i = 0; i < n; i++) {
-//         let comp = getComputerChoice();
-//         let user = getUserChoice();
-//         console.log(`computer shows:${comp}`);
-//         console.log(`User shows:${user}`);
-//         console.log(Result(comp, user));
-
-//     }
-//     if (UserCount > ComputerCount) {
-//         console.log(`User Wins , User Count: ${UserCount} , Computer Count: ${ComputerCount}`);
-//     }
-//     else if (UserCount < ComputerCount) {
-//         console.log(`Computer Wins , User Count: ${UserCount} , Computer Count: ${ComputerCount}`);
-//     }
-//     else {
-//         console.log(`Game Draw, User Count: ${UserCount} , Computer Count: ${ComputerCount}`);
-//     }
-// }
-
-// game();
+Result();
 
 
